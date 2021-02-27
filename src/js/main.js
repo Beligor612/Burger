12b227b06ea67d3adcbb163052df8357f3779231
Beligor612 <beligor612@gmail.com>
2021-02-27 17:57:04 +0200

    First-day pre-finish-listeners_for_buttons

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,13 @@
-// Mask for phone input
+// 
 $(document).ready(function(){
-    $('.phone-input').mask('+38 ( 099 ) 9999999')
+    
 })
 
 $(document).ready(function () {
+    // Mask for phone input
+    $('.phone-input').mask('+38 ( 099 ) 9999999')
+
+    // Counter listeners
     $('.counter_plus').click(function(e){
         let prevCounter = e.target.previousElementSibling.innerHTML
         e.target.previousElementSibling.innerHTML = Number(prevCounter) + 1
@@ -13,5 +17,21 @@ $(document).ready(function () {
         if(prevCounter != 0){
             e.target.nextElementSibling.innerHTML = Number(prevCounter) - 1
         }
+    })
+
+    // Button listeners
+    $('.order').click(function(){
+        $('.popup-mail').css('display', 'block')
+    })
+    $('.close_icon').click(function(e){
+        e.target.offsetParent.style.display = 'none'
+    })
+    $('.menu_icon').click(function(e){
+        console.log('JI')
+        $('.menu').css('display', 'block')
+    })
+    $('.call_me_back').click(function(e){
+        $('.menu').css('display', 'none')
+        $('.popup-phone').css('display', 'block')
     })
 });
