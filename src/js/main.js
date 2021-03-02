345790f834ca0830e4822ff0b62da1fc84ecf0f7
Beligor612 <beligor612@gmail.com>
2021-03-02 10:36:03 +0200

    fix the burger-calc

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,13 @@ $(document).ready(function(){
 })
 
 $(document).ready(function () {
-    let product = ''
-    let topHeight = 0
+    // Mask for phone input
+    $('.phone-input').mask('+38 ( 099 ) 9999999')
+
+    // Counter listeners
+    const burgerProductObj = {};
+    const burgerProductArray = []
+    let topHeight = 0;
     const objectSetting = {
         'Mayo': {
             y: -25 ,
@@ -48,12 +53,6 @@ $(document).ready(function () {
             addClassName: 'transform: rotate(-20deg) scale(.85)'
         },
     }
-    
-
-    // Mask for phone input
-    $('.phone-input').mask('+38 ( 099 ) 9999999')
-
-    // Counter listeners
     function params(name){
         const x = objectSetting[name].x
         const y = objectSetting[name].y
@@ -62,19 +61,30 @@ $(document).ready(function () {
     }
     function createElement(name, x, y, addClassName=''){
         return `
-        <div class="burger-form__item ${name.toLowerCase()}" style="position: absolute; top: ${y+topHeight}px;left: ${x}px;${addClassName}">
+        <div class="burger-form__item ${name.toLowerCase()} animate__animated animate__fadeIn" 
+            style="position: absolute; top: ${y+topHeight}px;left: ${x}px;${addClassName}">
             <img src="img/burger-form/${name}.png" alt="">
         </div>
     `
     }
+    function AddInfoProduct(name, prevCount){
+        burgerProductObj[name] = Number(prevCount) + 1
+        burgerProductArray.push(name)
+        return true
+    }
     $('.counter_plus').click(function(e){
         let prevCounter = e.target.previousElementSibling.innerHTML
         let name = e.target.parentElement.previousElementSibling.innerHTML
-        const parameters = params(name)
-        const newElement = createElement(name, ...parameters)
-        topHeight+= parameters[1]
+        AddInfoProduct(name, prevCounter)
+        const [x,y,addClassName] = params(name)
+        const newElement = createElement(name, x , y, addClassName)
+        topHeight+= y
         $('.burger-form-creator').append(newElement)
         e.target.previousElementSibling.innerHTML = Number(prevCounter) + 1
+        console.log(name)
+        if (name === 'Bun'){
+            $('.slider-card__counter .counter_plus').attr("disabled", true);
+        }
     })
     $('.counter_minus').click(function(e){
         let prevCounter = e.target.nextElementSibling.innerHTML
@@ -84,10 +94,16 @@ $(document).ready(function () {
             const array = $(`.${name.toLowerCase()}`)
             console.log(array)
             const len = array.length
+            const [,y] = params(name)
+            topHeight = topHeight + Math.abs(y)
             $(`.${name.toLowerCase()}`)[len-1].remove()
             e.target.nextElementSibling.innerHTML = Number(prevCounter) - 1
         }
+        if (name === 'Bun'){
+            $('.slider-card__counter .counter_plus').attr("disabled", false);
+        }
     })
+
     // Button listeners
     $('.order').click(function(){
         $('.popup-order').show()
@@ -95,6 +111,7 @@ $(document).ready(function () {
     $('.popup-order-ok').click(function(){
         $('.popum-mail').removeClass('animate__fadeIn')
         $('.popum-mail').addClass('animate__fadeOut')
+
         $('.popup-order').hide()
     })
     $('.close_icon-order').click(function(){
